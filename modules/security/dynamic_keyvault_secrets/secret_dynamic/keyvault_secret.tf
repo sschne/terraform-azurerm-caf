ac98957277b47504fde3cb7deb7b571509a0ddfc
Simon Schneider <simon.schneider@volocopter.com>
2023-03-21 12:39:48 +0000

    add urlencode to keyvault secrets

--- modules/security/dynamic_keyvault_secrets/secret_dynamic/keyvault_secret.tf
+++ modules/security/dynamic_keyvault_secrets/secret_dynamic/keyvault_secret.tf
@@ -4,9 +4,13 @@ resource "random_password" "value" {
   override_special = try(var.config.override_special, "_!@")
 }
 
+locals {
+  value = try(var.config.urlencode, false) ? urlencode(random_password.value.result) : random_password.value.result
+}
+
 resource "azurerm_key_vault_secret" "secret" {
   name         = var.name
-  value        = can(var.config.value_template) ? format(var.config.value_template, random_password.value.result)  : random_password.value.result
+  value        = can(var.config.value_template) ? format(var.config.value_template, local.value) : local.value
   key_vault_id = var.keyvault_id
 
   lifecycle {
--- modules/security/dynamic_keyvault_secrets/secret_immutable/keyvault_secret.tf
+++ modules/security/dynamic_keyvault_secrets/secret_immutable/keyvault_secret.tf
@@ -1,7 +1,11 @@
+locals {
+  value = try(var.config.urlencode, false) ? urlencode(var.value) : var.value
+}
+
 resource "azurerm_key_vault_secret" "secret" {
   count        = var.value == "" ? 1 : 0
   name         = var.name
-  value        = can(var.config.value_template) ? format(var.config.value_template, var.value)  : var.value
+  value        = can(var.config.value_template) ? format(var.config.value_template, local.value) : local.value
   key_vault_id = var.keyvault_id
 
   lifecycle {
